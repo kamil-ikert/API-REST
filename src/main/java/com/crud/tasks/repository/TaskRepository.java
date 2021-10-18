945923e88288f23b75f72bc0190c7cc51c0b6ed1
Kamil Ikert <kamil.ikert@gmail.com>
2021-10-18 16:22:02 +0200

    commit 2

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -13,8 +13,11 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     List<Task> findAll();
 
     @Override
+    Optional<Task> findById(Long id);
+
+    @Override
     Task save(Task task);
 
     @Override
-    Optional<Task> findById(Long id);
+    void deleteById(Long id);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -2,21 +2,19 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class DbService {
 
-    @Autowired
     private final TaskRepository repository;
-
-    public DbService(TaskRepository repository) {
-        this.repository = repository;
-    }
 
     public List<Task> getAllTasks() {
         return repository.findAll();
